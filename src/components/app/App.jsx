3873eb0f32cfb0a118660b38a174de32e2709e77
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-05-28 13:37:43 -0700

    clean up code and removed console logs

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,14 @@
+/* eslint-disable indent */
 /* eslint-disable max-len */
 import React, { useEffect, useState } from 'react';
 
 const useRecord = () => {
   const [current, setCurrent] = useState(0);
-  const [colorHistory, addToHistory] = useState(['#FF0000']);
+  const [colorHistory, addToHistory] = useState([]);
 
   useEffect(() => {
-    console.log('current index - useEffect', current);
-    console.log('current history array - useEffect', colorHistory);
+    // console.log('current index - useEffect', current);
+    // console.log('current history array - useEffect', colorHistory);
   }, [current]);
 
   const undo = () => {
@@ -18,25 +19,16 @@ const useRecord = () => {
     setCurrent((prev) => prev + 1);
   };
 
-  // const record = (clr) => {
-  //   console.log('chosen color', clr);
-  //   console.log('current index', current);
-  //   //this just adds it to the end, we need to add it in at the index given.
-  //   addToHistory((prev) => [...prev, clr]);
-  //   console.log('current history array', colorHistory);
-  //   setCurrent((prev) => prev + 1);
-  //   console.log('current index', current);
-  // };
-
   const record = (clr) => {
     if (colorHistory.length === current + 1) {
       addToHistory((prev) => [...prev, clr]);
       setCurrent((prev) => prev + 1);
     }
     else {
-      addToHistory((prevArr) => [
-        ...prevArr.slice(0, current + 1), // ...before
-        clr,
+      addToHistory((prevArr) =>
+        // prevArr.splice(current + 1, 0, clr));
+        [...prevArr.slice(0, current + 1), // ...before
+          clr,
         ...prevArr.slice(current + 1)]); // ...after
       setCurrent((prev) => prev + 1);
     }
@@ -57,7 +49,7 @@ function App() {
       </div>
       <label /> Choose A Color:
       <input type="color" style={{ margin: '10px' }} value={colorHistory[current]} onChange={(e) => record(e.target.value)} />
-      <div style={{ backgroundColor: colorHistory[current], width: '10rem', height: '10rem', margin: '10px' }}></div>
+      {if(colorHistory.length ? <div style={{ backgroundColor: colorHistory[current], width: '10rem', height: '10rem', margin: '10px' }}></div> : '')}
     </div>
   );
 }
