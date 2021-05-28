292c9228b9d0c3b55f00f458cf4e37a481870005
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-05-28 15:17:22 -0700

    initial page element test and screenshot test passing

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -42,22 +42,45 @@ function App() {
   const { current, undo, redo, record, colorHistory } = useRecord();
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
       <div>
         <button
+          aria-label={'undo'}
           onClick={undo}
           style={{ margin: '10px' }}
           disabled={current <= 0}
         >undo</button>
         <button
+          aria-label={'redo'}
           onClick={redo}
           style={{ margin: '10px' }}
           disabled={current >= colorHistory.length - 1 || colorHistory.length <= 1}
         >redo</button>
       </div>
-      <label /> Choose A Color:
-      <input type="color" style={{ margin: '10px' }} value={colorHistory[current]} onChange={(e) => record(e.target.value)} />
-      <div style={{ backgroundColor: colorHistory[current], width: '10rem', height: '10rem', margin: '10px' }}></div>
+      <label> Choose A Color:
+
+      <input
+          aria-label={'color-input'}
+          type="color"
+          style={{ margin: '10px' }}
+          value={colorHistory[current]}
+          onChange={(e) => record(e.target.value)}
+        />
+      </label>
+      <div
+        style={{
+          backgroundColor: colorHistory[current],
+          width: '10rem',
+          height: '10rem',
+          margin: '10px'
+        }}
+      ></div>
     </div>
   );
 }
--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { getByRole, render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
 import App from './App';
 
 describe('App component', () => {
   it('renders App', () => {
-    render(<App />);
-    screen.getByRole('button', { name: 'undo' });
+    const app = render(<App />);
+    expect(app).toMatchSnapshot();
+    //on load
+    const undo = screen.getByRole('button', { name: 'undo' });
+    expect(undo).toHaveAttribute('disabled');
+    const redo = screen.getByRole('button', { name: 'redo' });
+    expect(redo).toHaveAttribute('disabled');
+    const colorInput = screen.getByLabelText('Choose A Color:');
+    expect(colorInput).toHaveValue('#ffffff');
+
+    // fireEvent.input(colorInput, { target: { value: '#FF0000' } });
 
   });
 });
+
+// select #FF0000 
+// Expect the square change to #FF0000
+// select #0000FF 
+// Expect the square change to #0000FF
+// select #00FF00
+// Expect the square change to #00FF00
+// click undo
+// Expect the square change to #0000FF
+// press undo
+// Expect the square change to #FF0000
+// press redo
+// Expect the square change to #0000FF
+// select #FFFF00
+// Expect the square change to #FFFF00
+// press undo
+// Expect the square change to #0000FF
+// press undo
+// Expect the square change to #FF0000
+// press redo
+// Expect the square change to #0000FF
+// press redo
+// Expect the square change to #FFFF00
+// press redo
+// Expect the square change to #00FF00
